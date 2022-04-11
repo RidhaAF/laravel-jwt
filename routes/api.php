0ac07e9bed67286fde4520b40aa7a722d7d568d2
ridhaaf <ridhaaf@gmail.com>
2022-04-12 01:00:02 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['middleware' => 'api'], function ($router) {
+Route::group(['middleware' => 'api'], function ($router) {
     Route::post('/register', [JWTController::class, 'register']);
     Route::post('/login', [JWTController::class, 'login']);
     Route::post('/logout', [JWTController::class, 'logout']);
